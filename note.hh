23b2362a58fada9db42b9580e52352070560d6d2
Bmeshanko <bmeshank@purdue.edu>
2023-06-25 20:17:20 -0400

    Removing the pre-generated notes, we are using pitch

--- note.hh
+++ note.hh
@@ -37,16 +37,3 @@ class Note {
 /*
 *   Guitars are tuned where the lowest note is E, which we are setting to pitch 0.
 */
-
-Note E = Note(0);
-Note F = Note(1);
-Note Gb = Note(2);
-Note G = Note(3);
-Note Ab = Note(4);
-Note A = Note(5);
-Note Bb = Note(6);
-Note B = Note(7);
-Note C = Note(8);
-Note Db = Note(9);
-Note D = Note(10);
-Note Eb = Note(11);
